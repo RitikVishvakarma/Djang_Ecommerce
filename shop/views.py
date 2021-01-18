79ebd89a6d72b71702e506958e60a6d4c4357975
Ritik <ritikvishvakarma2000@gmail.com>
2021-01-18 12:29:38 +0530

    Creating models for products

--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("This is about")
+
+def contact(request):
+    return HttpResponse("This is contact")
+
+def tracker(request):
+    return HttpResponse("This is tracker")
+
+def search(request):
+    return HttpResponse("This is search")
+
+def productView(request):
+    return HttpResponse("This is prodView")
+
+def checkout(request):
+    return HttpResponse("This is checkout")
