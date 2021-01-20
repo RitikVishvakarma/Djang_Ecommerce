05d827458271ffc20fd0e2952c4a980aacd40b62
Ritik <ritikvishvakarma2000@gmail.com>
2021-01-20 14:24:04 +0530

    category wise product display

--- shop/views.py
+++ shop/views.py
@@ -4,10 +4,20 @@ from math import ceil
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'product': products, 'no_of_slide':nSlides, 'range':range(1,nSlides)}
+    # products = Product.objects.all()
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    # params = {'product': products, 'no_of_slide':nSlides, 'range':range(1,nSlides)}
+    # allProds = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
 def about(request):
