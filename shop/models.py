1e30a13d4d82dd28587a84a445e6e806d68cbf83
Ritik <ritikvishvakarma2000@gmail.com>
2021-01-23 16:04:27 +0530

    Creating contact page and model for contact

--- shop/models.py
+++ shop/models.py
@@ -12,3 +12,13 @@ class Product(models.Model):
     image = models.ImageField(upload_to="shop/images", default="")
     def __str__(self):
         return self.product_name
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+    desc = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
