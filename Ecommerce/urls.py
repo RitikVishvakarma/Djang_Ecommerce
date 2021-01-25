dc71f6e951c4cf67ff8e5e74eaff595beed87409
Ritik <ritikvishvakarma2000@gmail.com>
2021-01-25 14:49:28 +0530

    Creating Order Tracker for customers

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
+import json
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    # products = Product.objects.all()
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
     allProds = []
     catProds = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catProds}
@@ -15,8 +14,6 @@ def index(request):
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
-    # params = {'product': products, 'no_of_slide':nSlides, 'range':range(1,nSlides)}
-    # allProds = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
@@ -34,6 +31,22 @@ def contact(request):
     return render(request, 'shop/contact.html')
 
 def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
     return render(request, 'shop/tracker.html')
 
 def search(request):
@@ -56,6 +69,8 @@ def checkout(request):
 
         order = Orders(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
         order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
         thank=True
         id=order.order_id
         return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
