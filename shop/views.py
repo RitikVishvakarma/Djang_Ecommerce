1031eafbf9720a1dc38e00e4eeb148df216cb5e7
Ritik <ritikvishvakarma2000@gmail.com>
2021-01-28 10:28:08 +0530

    Creating product search feature

--- shop/views.py
+++ shop/views.py
@@ -19,6 +19,29 @@ def index(request):
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
+def searchMatch(query, item):
+    if query in item.des.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) !=0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds, 'msg':""}
+    if len(allProds) == 0 or len(query)< 2:
+        params = {'msg':"Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
 def about(request):
     return render(request, 'shop/about.html')
 
@@ -52,9 +75,6 @@ def tracker(request):
         except Exception as e:
             return HttpResponse('{}')
     return render(request, 'shop/tracker.html')
-
-def search(request):
-    return render(request, 'shop/search.html')
 
 def productView(request, myid):
     product = Product.objects.filter(id=myid)
